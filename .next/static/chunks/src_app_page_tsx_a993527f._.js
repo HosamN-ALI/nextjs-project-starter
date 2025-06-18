74c5bccbd80477b8c83b3201040333122bf95d2d
HosamN-ALI <93358299+HosamN-ALI@users.noreply.github.com>
2025-06-19 02:39:55 +0300

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1750289995817

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_96797649._.js",
+  "static/chunks/src_69c0bc38._.js"
+],
+    source: "dynamic"
+});
